c2bd6be8c7066cc7ea3f34eb91aa124aa358a218
Ann-BB <Beruashviliani35@gmail.com>
2024-05-14 00:44:53 +0400

    header change

--- src/app/content/header/header-content/header-content.component.ts
+++ src/app/content/header/header-content/header-content.component.ts
@@ -1,63 +1,53 @@
-import { Component, ElementRef, HostListener, Inject, OnDestroy, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderListComponent } from '../header-list/header-list.component';
 import { HeaderIconsComponent } from '../header-icons/header-icons.component';
 import { TranslateModule } from '@ngx-translate/core';
-import { CommonModule, DOCUMENT } from '@angular/common';
-import { Observable, Subscription, debounceTime, distinctUntilChanged, filter, fromEvent, map, pairwise, tap } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { debounceTime, distinctUntilChanged, fromEvent, map } from 'rxjs';
 import { RouterModule } from '@angular/router';
 import { GeneralService } from '../../../service/general.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header-content',
   standalone: true,
-  imports: [CommonModule, TranslateModule, RouterModule, HeaderListComponent, HeaderIconsComponent],
+  imports: [CommonModule, TranslateModule, RouterModule, HeaderListComponent, HeaderIconsComponent, MatSidenavModule, MatButtonModule],
   templateUrl: './header-content.component.html',
   styleUrl: './header-content.component.scss'
 })
 export class HeaderContentComponent {
-  // public sticky$?: Observable<boolean>;
   fixed: boolean = false
-  resizeSubscription$!: Subscription;
-  resizeWidth?: number;
-  ngOnInit() {
+  SideBarShow = false;
 
-    this.resizeWidth = window.innerWidth;
+  ngOnInit() {
   }
-  ngAfterViewInit() { 
+
+  ngAfterViewInit() {
     this.handleScroll();
-    this.handleResize()
   }
+
   constructor(
-    public generalService: GeneralService, 
-  ){ }
+    public generalService: GeneralService,
+  ) { }
 
   handleScroll() {
     fromEvent(document, "scroll", { passive: false }).pipe(
-      debounceTime(50), 
-      map(e => window.pageYOffset),  
+      debounceTime(50),
+      map(e => window.pageYOffset),
       distinctUntilChanged()
     ).subscribe(res => {
       if (Math.abs(res) > 0) {
         this.fixed = true;
       } else if (Math.abs(res) == 0) {
         this.fixed = false;
-      } 
+      }
     });
   }
 
-  private handleResize() {
-    this.resizeSubscription$ = fromEvent(window, 'resize') 
-      .pipe(
-        map(() => window.innerWidth),
-        distinctUntilChanged(),
-        debounceTime(1000)
-      )
-      .subscribe(() => { 
-        this.resizeWidth = window.innerWidth;
-      });
-  } 
-
-  ngOnDestroy() {
-    this.resizeSubscription$.unsubscribe()
+  toggleSideBar() {
+    this.SideBarShow = !this.SideBarShow;
+    console.log(this.SideBarShow, "SideBarShow")
   }
+
 }
